f01454ace9bae2db619e812a26bf6a1038b6d398
Mylena <myvendramini@gmail.com>
2023-07-26 09:57:37 +0100

    feat: add generate-csv-report route

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,13 +2,14 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
+const { createCsvRows, getRequest, postRequest } = require("./helperFunctions")
 
 const app = express()
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }))
 
 app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
+  const { id } = req.params
   request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
     if (e) {
       console.error(e)
@@ -17,6 +18,27 @@ app.get("/investments/:id", (req, res) => {
       res.send(investments)
     }
   })
+})
+
+app.get("/generate-csv-report/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    if (!id || isNaN(id)) return res.status(400).send("Invalid investment id")
+
+    const userInvestments = await getRequest(`${config.investmentsServiceUrl}/investments/${id}`)
+    if (!userInvestments || userInvestments.length === 0) return res.status(404).send("Information not found")
+
+    const companies = await getRequest(`${config.financialCompaniesUrl}/companies`)
+    if (!companies || companies.length === 0) return res.status(404).send("Information not found")
+
+    const csvReport = createCsvRows(userInvestments[0], companies)
+    await postRequest(`${config.investmentsServiceUrl}/investments/export`, csvReport)
+    res.type("text/csv")
+    res.send(csvReport)
+  } catch (e) {
+    console.error("Error occurred while generating CSV report:", e)
+    res.status(500).send("Internal Server Error")
+  }
 })
 
 app.listen(config.port, (err) => {
