8fcfa401f47eff4a4aae3c26322e49cb383ff93b
Mylena <myvendramini@gmail.com>
2023-07-26 10:37:27 +0100

    feat: add test for createCsvRows function

--- admin/tests/index.test.js
+++ admin/tests/index.test.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { describe, expect, test, afterAll } = require("@jest/globals")
 const { mockCompanies, mockUserInvestment } = require("./mocks")
-const { findHoldingNameById, calculateInvestmentValue } = require("../src/helperFunctions")
+const { findHoldingNameById, calculateInvestmentValue, createCsvRows } = require("../src/helperFunctions")
 const PORT = 8084
 
 const app = express()
@@ -80,6 +80,30 @@ describe("Admin service", () => {
       investmentTotal = 150000
       investmentPercentage = "0.2"
       expect(calculateInvestmentValue(investmentTotal, investmentPercentage)).toBe(0)
+    })
+  })
+  describe("Create CSV Rows function", () => {
+    test("should create CSV rows correctly", () => {
+      const result = createCsvRows(mockUserInvestment, mockCompanies)
+      const expected = { "csv": "|User|First Name|Last Name|Date|Holding|Value|\n|2|Sheila|Aussie|2020-03-01|The Big Investment Company|10750|\n|2|Sheila|Aussie|2020-03-01|The Small Investment Company|6450|\n|2|Sheila|Aussie|2020-03-01|Capital Investments|4300|" }
+      expect(result).toEqual(expected)
+    })
+    test("should return empty CSV for empty or null userInvestments", () => {
+      const expected = { "csv": "|User|First Name|Last Name|Date|Holding|Value|" }
+      expect(createCsvRows("", mockCompanies)).toEqual(expected)
+      expect(createCsvRows(null, mockCompanies)).toEqual(expected)
+    })
+    test("should return empty CSV for empty or null companies", () => {
+      const expected = { "csv": "|User|First Name|Last Name|Date|Holding|Value|" }
+      expect(createCsvRows(mockUserInvestment, [])).toEqual(expected)
+      expect(createCsvRows(mockUserInvestment, null)).toEqual(expected)
+    })
+    test("should return empty CSV for both empty or null companies and userInvestments", () => {
+      const expected = { "csv": "|User|First Name|Last Name|Date|Holding|Value|" }
+      expect(createCsvRows("", [])).toEqual(expected)
+      expect(createCsvRows(null, null)).toEqual(expected)
+      expect(createCsvRows(null, [])).toEqual(expected)
+      expect(createCsvRows("", null)).toEqual(expected)
     })
   })
 })
